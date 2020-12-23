2a1abe656afa7637a6ddc14a7d8275a9258e5e14
sawich <sawich@angeldev.org>
2020-12-23 08:22:38 +0200

    github events channel

--- src/defines.ts
+++ src/defines.ts
@@ -1,10 +1,13 @@
 export const defines = {
-  roles: [{
+  roles: [{ // git
     emoji: '785672410242351124',
     role: '787207100044279808',
-  }, {
+  }, { // thitch
     emoji: '791113170470830140',
     role: '787207068841934848',
+  }, { // git events
+    emoji: '785672410803863574',
+    role: '791186577057382404',
   }],
 
   channels: {
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 config();
 
 import { defines } from './defines';
-import Discord, { Message } from 'discord.js';
+import Discord from 'discord.js';
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
 
@@ -10,15 +10,9 @@ client.on('ready', () => {
   console.log('Bot has started');
 });
 
-client.on('message', (message: Message) => {
-});
-
 client.on('error', (e) => {
   console.error('Discord client error!', e);
 });
-
-
-
 
 const main = async () => {
   await client.login(process.env.DISCORD_TOKEN);
@@ -26,7 +20,11 @@ const main = async () => {
   const channel: Discord.TextChannel = await (client.channels.fetch(defines.channels.entryPoint) as Promise<Discord.TextChannel>);
   const message = await channel.messages.fetch(defines.messages.helloMessage);
 
-  const messageCollector = message.createReactionCollector((reaction: Discord.MessageReaction) => defines.roles.some(c => c.emoji == reaction.emoji?.id), { dispose: true });
+  const messageCollector = message.createReactionCollector((reaction: Discord.MessageReaction) => {
+    console.log(reaction.emoji.id);
+
+    return defines.roles.some(c => c.emoji == reaction.emoji?.id);
+  }, { dispose: true });
 
   messageCollector.on('remove', async (reaction, user) => {
     console.log('remove');
